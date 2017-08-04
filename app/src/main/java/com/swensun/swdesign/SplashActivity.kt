8c8ccefe6ab0e2ee085bde4f531f834f518e8d9a
sunwen <1416316137@qq.com>
2017-08-04 11:05:06 +0800

    add navigationView

--- app/src/main/java/com/swensun/swdesign/SplashActivity.kt
+++ app/src/main/java/com/swensun/swdesign/SplashActivity.kt
@@ -1,8 +1,12 @@
 package com.swensun.swdesign
 
 
+import android.content.Intent
 import android.os.Bundle
 import com.swensun.swdesign.base.SwAppCompatActivity
+import com.swensun.swdesign.ui.MainActivity
+import io.reactivex.Flowable
+import java.util.concurrent.TimeUnit
 
 
 class SplashActivity : SwAppCompatActivity() {
@@ -14,10 +18,10 @@ class SplashActivity : SwAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-//        Flowable.timer(3, TimeUnit.SECONDS)
-//                .subscribe {
-//                    startActivity()
-//                }
+        Flowable.timer(500, TimeUnit.MICROSECONDS)
+                .subscribe {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                }
     }
 }
-
